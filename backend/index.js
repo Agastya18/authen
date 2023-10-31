2cf4661f8459e546d60730e1541de2f0af1b7565
agastya gaur <agastyagaur17@gmail.com>
2023-10-31 23:27:52 +0530

    add database(mongodb)

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
+const path=require('path');
+require('dotenv').config({path:path.resolve(__dirname, './.env')});
 const express = require('express');
 const app = express();
-
-
+const connectDB = require('./database/db');
+connectDB();
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
