b04909facd456e67e08c71825fbfe54f2a7f0c2f
agastya gaur <agastyagaur17@gmail.com>
2023-10-31 23:52:33 +0530

    add model schema

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const connectDB = require('./database/db');
 connectDB();
+app.get('/', (req, res) =>{
+    res.send("hi from server")
+})
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        
+    },
+    
+    
+    
+},{timestamps: true})
+const User=new mongoose.Model('User',userSchema);
+export default User;
