d7f8cad088b53aa75de2086356821fce06c48c32
agastya gaur <agastyagaur17@gmail.com>
2023-11-05 13:54:49 +0530

    add persist data

--- client/src/compoments/Header.jsx
+++ client/src/compoments/Header.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom"
-
+import { useSelector } from "react-redux"
 
 const Header = () => {
+  const currentUser= useSelector(state=>state.persistedReducer.user.currentUser)
+  console.log(currentUser.profilePic)
   return (
     <div className=' bg-slate-200'>
         <div className=" flex  justify-around">
           <h1 className="  text-2xl p-5 font-bold">AUTHer</h1>
             <ul className="flex  gap-4 items-center text-2xl">
             <Link to={"/"}> <li>Home</li></Link>
-               <Link  to="/Signin"> <li>Signin</li></Link>
+              
                <Link to={"/about"}><li>About</li></Link> 
+               <Link  to="/Signin"> 
+               {currentUser ? (
+                <img src={currentUser.profilePic} className=" h-10 w-10 rounded-full"/>
+               ):( <li>Sign In</li>)}
+               
+              
+               </Link>
                
             </ul>
         </div>
